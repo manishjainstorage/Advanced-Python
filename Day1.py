5258924a2fdeafb2809238f0b6b53a3e501c4dd8
Dr.Manish Jain <33494092+manishjainstorage@users.noreply.github.com>
2024-09-30 17:13:34 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -3,5 +3,25 @@ import numpy as np
 df = pd.read_csv("/content/Ajio.csv" , encoding = "unicode_escape")
 df.head()
 
+df.info()
+df.describe()
+df.describe(include=[object])
+
+######################################################################3
+import sweetviz as sv
+import pandas as pd
+
+# Load your dataset
+df = pd.read_csv("/content/Ajio.csv" , encoding = "Unicode_escape")
+
+# Generate the report
+report = sv.analyze(df)
+
+# Display the report
+report.show_html("Sweetviz_Report.html")
+########################################################################
+
+
+
 
 

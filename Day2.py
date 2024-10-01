7fd6175bafe0c500ef923607d4f49a2b8e32a169
Dr.Manish Jain <33494092+manishjainstorage@users.noreply.github.com>
2024-10-01 16:43:29 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -6,3 +6,31 @@ import pandas as pd
 print(pd.__version__)
 
 !pip show pandas
+__________________________________________________________________________________
+import pandas as pd
+
+# https://github.com/manishjainstorage/Advanced-Python/blob/main/StockMarketData-13krows.xlsx
+# Define the correct raw URL of the .xlsx file
+url = "https://github.com/manishjainstorage/Advanced-Python/raw/main/StockMarketData-13krows.xlsx"
+
+# Read the .xlsx file into a DataFrame, specifying the engine
+df = pd.read_excel(url, engine='openpyxl')
+
+# Display the first few rows of the DataFrame
+df.head()
+__________________________________________________________________________________
+
+import pandas as pd
+import polars as pl
+import time
+
+# Load large dataset using Pandas
+start_time = time.time()
+df_pandas = pd.read_excel("https://github.com/manishjainstorage/Advanced-Python/raw/main/StockMarketData-13krows.xlsx")
+print(f'Pandas load time: {time.time() - start_time} seconds')
+
+# Load large dataset using Polars
+start_time = time.time()
+df_polars = pl.read_excel("https://github.com/manishjainstorage/Advanced-Python/raw/main/StockMarketData-13krows.xlsx")
+print(f'Polars load time: {time.time() - start_time} seconds')
+___________________________________________________________________________________

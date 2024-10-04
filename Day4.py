30d8183d2d52fc20d6bb08aa9301c1cf6a8d3b2c
Dr.Manish Jain <33494092+manishjainstorage@users.noreply.github.com>
2024-10-04 18:48:12 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -6,3 +6,21 @@ fig.update_layout(
     yaxis_title="Number of Passengers"
 )
 fig.show()
+
+
+###########################################################
+
+import streamlit as st
+import pandas as pd 
+import numpy as np 
+st.title("My APP")
+userinput = st.text_input("Enter a name","streamlit user")
+st.write(f"Hello,{userinput}") 
+data = np.random.randn(100,3) 
+df = pd.DataFrame(data,columns=['A','B','C'])
+st.line_chart(df)
+num = st.slider('select a no',0,100,25) 
+st.write(f"You selected: {num}")
+____________________________________________________________
+
+streamlit run app.py

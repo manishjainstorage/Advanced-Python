53f6236e5a2f514a69fa7c67d0397a3df07c8cfc
Dr.Manish Jain <33494092+manishjainstorage@users.noreply.github.com>
2024-10-04 17:12:05 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,1 +1,8 @@
 
+import plotly.express as px
+
+fig = px.line(flights, x="month", y="passengers", color="year", title="Number of Passengers Over Time")
+fig.update_layout(
+    yaxis_title="Number of Passengers"
+)
+fig.show()
